4a8c8e1151550e008ae1e62d323bac2f771ce154
Laiqa <laiqanr213@gmail.com>
2025-10-30 23:32:07 +0800

    corrected css path

--- admin/admin_home.php
+++ admin/admin_home.php
@@ -16,8 +16,8 @@ $totalPayments = $pdo->query("SELECT SUM(amount) FROM payments")->fetchColumn();
 ?>
 <!doctype html>
 <html><head><meta charset="utf-8"><title>Admin Home</title>
-<link rel="stylesheet" href="/matangreads/css/style.css"> 
-<link rel="stylesheet" href="/matangreads/css/admin.css">
+<link rel="stylesheet" href="../css/style.css"> 
+<link rel="stylesheet" href="../css/admin.css">
 
 <style>
 
